fc1c0ea5c65ed43373d6ea71576403e892808d53
cavpollo <cavpollo@gmail.com>
2018-01-12 09:50:19 -0600

    Add spin up and down workers

--- spin_down.js
+++ spin_down.js
@@ -4,8 +4,8 @@ const heroku = new Heroku({token: process.env.HEROKU_API_TOKEN})
 heroku.patch('/apps/' + process.env.APP_NAME + '/formation/worker', {
     body: {
         'quantity': 0,
-        'size': 'hobby'
-    }
+        'size': 'hobby',
+    },
 }).then(app => {
     console.log('SpinDown:')
     console.log(app)
--- spin_up.js
+++ spin_up.js
@@ -4,8 +4,8 @@ const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
 heroku.patch('/apps/' + process.env.APP_NAME + '/formation/worker', {
     body: {
         'quantity': 1,
-        'size': 'hobby'
-    }
+        'size': 'hobby',
+    },
 }).then(app => {
     console.log('SpinDown:')
     console.log(app)
